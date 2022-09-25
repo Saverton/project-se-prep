8acf40a8ce39b48647855f0e14387fc05681f1a8
Scott Meadows <Scottmeadows04@gmail.com>
2022-09-25 16:09:11 -0400

    add structure headings, remove test styling and script

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
 // test creation of an h2 element to ensure that our JavaScript script is functioning correctly.
-const h2 = document.createElement("h2");
-h2.textContent = "The JavaScript script is working!";
-document.querySelector("body").appendChild(h2);
