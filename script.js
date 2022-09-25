3b3c3e83a6afa1930ee661614c734e5a1b38edd8
Scott Meadows <Scottmeadows04@gmail.com>
2022-09-25 15:50:40 -0400

    rename script, add basic script

---
+++ script.js
@@ -0,0 +1,4 @@
+// test creation of an h2 element to ensure that our JavaScript script is functioning correctly.
+const h2 = document.createElement("h2");
+h2.textContent = "The JavaScript script is working!";
+document.querySelector("body").appendChild(h2);
